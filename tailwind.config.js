61f6ced9cd4b4593abf1f972d61d8bb1144f3a16
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-02-05 20:48:12 +0100

    added default styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      black: "hsl(0, 0%, 0%)",
+      "light-gray": "hsl(0, 0%, 98%)",
+      gray: "hsl(0, 0%, 95%)",
+      "very-dark": "hsl(0, 0%, 6%)",
+      brown: "hsl(22, 65%, 57%)",
+      orange: "hsl(21, 94%, 75%)",
+      error: "hsl(0 , 65%, 49%)",
+    },
+    fontFamily: {
+      primary: ["Manrope", "sans-serif"],
+    },
+    fontWeight: { bold: 700, medium: 500, regular: 400 },
+    screens: { tablet: "34rem", desktop: "54rem" },
     extend: {},
   },
   plugins: [],
