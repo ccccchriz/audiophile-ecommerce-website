999a5b53e2deb14ff687cf8e27e2ac7f48372df4
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-02-07 17:21:33 +0100

    added main layout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <div className="min-h-dvh flex flex-col">
+      <Header />
+      <main className="font-primary grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
