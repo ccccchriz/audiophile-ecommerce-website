adeb2b415975a8caa93e44342201f8c1dba7ffd6
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-02-07 20:01:39 +0100

    added h1

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,7 +9,10 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="min-h-dvh flex flex-col">
       <Header />
-      <main className="font-primary grow">{children}</main>
+      <main className="font-primary grow">
+        <h1 className="sr-only">Audiophile Store</h1>
+        {children}
+      </main>
       <Footer />
     </div>
   );
