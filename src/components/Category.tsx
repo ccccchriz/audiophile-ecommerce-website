d86835a71f2443cd9a4e14ad08e29499e1f1f944
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-02-08 20:43:08 +0100

    fixed focus state

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -8,13 +8,13 @@ export default function Category({ image, title, link }: CategoryProps) {
   return (
     <a
       href={link}
-      className="group bg-gray flex flex-col justify-end items-center uppercase mt-20 rounded-lg p-4 max-h-40 w-full hover:bg-opacity-50"
+      className="group bg-gray flex flex-col justify-end items-center uppercase mt-20 rounded-lg p-4 max-h-40 w-full hover:bg-opacity-50 focus-visible:bg-opacity-50"
     >
       <img src={image} alt="" className="max-w-40 -mt-20 " />
       <span className="font-bold tracking-widest text-lg">{title}</span>
       <span
         aria-hidden={true}
-        className="text-sm tracking-widest flex gap-2 items-center group-hover:text-brown"
+        className="text-sm tracking-widest flex gap-2 items-center group-hover:text-brown group-focus-visible:text-brown"
       >
         shop
         <img
