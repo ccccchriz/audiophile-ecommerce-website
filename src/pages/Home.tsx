ce5e0341c5add852fe045fe1eb51fe90682a2683
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-02-13 09:29:00 +0100

    updated speaker card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,7 +57,15 @@ export default function Home() {
       </div>
       <div className="p-6 w-full max-w-[80rem] desktop:px-0">
         <div className="flex flex-col items-center text-white text-center gap-6 bg-brown w-full rounded-lg px-6 py-12 bg-[url(/images/home/desktop/pattern-circles.svg)] bg-no-repeat bg-[center_top_-10rem] bg-[length:43rem] desktop:grid desktop:grid-cols-2 desktop:text-left desktop:overflow-hidden">
-          <picture className="max-w-52 desktop:row-[1/4] desktop:self-end desktop:justify-self-center -mb-14">
+          <picture className="max-w-52 desktop:row-[1/4] desktop:self-end desktop:justify-self-center desktop:-mb-14">
+            <source
+              media="(min-width: 54rem)"
+              srcSet="/images/home/desktop/image-speaker-zx9.png"
+            />
+            <source
+              media="(min-width: 34rem)"
+              srcSet="/images/home/tablet/image-speaker-zx9.png"
+            />
             <img
               src="/images/home/mobile/image-speaker-zx9.png"
               alt=""
@@ -71,7 +79,7 @@ export default function Home() {
           </p>
           <a
             href=""
-            className="uppercase bg-very-dark  px-8 py-4 w-max tracking-wider"
+            className="uppercase bg-very-dark px-8 py-4 w-max tracking-wider hover:bg-[#4C4C4C] focus-visible:bg-[#4C4C4C]"
           >
             See product
           </a>
