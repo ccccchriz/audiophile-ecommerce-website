1a066fa0e565fc65f6bc34d34886b05b08accd95
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-02-17 17:01:35 +0100

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Home />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
