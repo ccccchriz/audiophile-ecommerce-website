4b50ed3134dc3cffe031b131a9404836a8836ade
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-02-18 15:09:55 +0100

    react router adjustments and added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Catergory from "./pages/Category";
+import MainLayout from "./layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
-    path: "*",
-    element: <Home />,
+    path: "/",
+    element: <MainLayout />,
+    loader: async () => {
+      return fetch("/data.json").then((data) => data.json());
+    },
+    id: "root",
+    children: [
+      {
+        path: "category",
+        element: <Catergory />,
+      },
+
+      {
+        path: "",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
