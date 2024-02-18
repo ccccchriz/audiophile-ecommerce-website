c21148dcf0189a61ae43f8cf21f5fec2984c9d24
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-02-18 17:14:30 +0100

    added ts types

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Catergory from "./pages/Category";
 import MainLayout from "./layouts/MainLayout";
+import dataType from "./utilities/dataType";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    loader: async () => {
+    loader: async (): Promise<dataType> => {
       return fetch("/data.json").then((data) => data.json());
     },
     id: "root",
     children: [
       {
-        path: "category",
+        path: "category/:category",
         element: <Catergory />,
       },
 
