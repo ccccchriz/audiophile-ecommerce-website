bcb8257f61260724ebd0b1e6e871876a669a8625
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-03-02 23:13:57 +0100

    created item list

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,6 +5,24 @@ interface CartProps {
   setIsCartExpanded: Function;
 }
 
+interface cartType {
+  item: string;
+  amount: number;
+  price: number;
+  image: string;
+}
+
+// TEMP
+
+const data: cartType[] = [
+  {
+    item: "ZX9 Speaker",
+    amount: 1,
+    price: 4500,
+    image: "/images/product-xx59-headphones/mobile/image-product.jpg",
+  },
+];
+
 export default function Cart({ isCartExpanded, setIsCartExpanded }: CartProps) {
   const cart = useRef<HTMLDialogElement>(null);
 
@@ -16,7 +34,7 @@ export default function Cart({ isCartExpanded, setIsCartExpanded }: CartProps) {
   return (
     <dialog ref={cart} className="mt-24 max-w-[70rem] bg-[transparent] w-full">
       <div className="w-full grid desktop:grid-cols-2 p-6">
-        <div className="grid w-full p-6 desktop:col-[2/3] bg-white rounded-lg">
+        <div className="grid gap-6 w-full p-6 desktop:col-[2/3] bg-white rounded-lg">
           <button
             type="button"
             onClick={() => {
@@ -37,17 +55,46 @@ export default function Cart({ isCartExpanded, setIsCartExpanded }: CartProps) {
           <div className="flex">
             <h2>CART</h2>
             <p>
-              (<span>1</span>)
+              (<span>{data.length}</span>)
             </p>
             <button type="button" className="ml-auto">
               Remove All
             </button>
           </div>
           <div className="">
+            {data.length > 0 ? (
+              data.map((el, index) => (
+                <div key={index} className="flex gap-4 items-center">
+                  <picture className="rounded-lg overflow-hidden">
+                    <img src={el.image} alt="" className="w-full max-w-16" />
+                  </picture>
+                  <div className="grow">
+                    <h2 className="font-bold">{el.item}</h2>
+                    <p className="tracking-wide font-bold opacity-50">
+                      $ {el.price}
+                    </p>
+                  </div>
+
+                  <div className="flex items-center gap-2 bg-gray">
+                    <button className="py-2 px-3 font-bold  text-xl h-full disabled:opacity-35 [&:not(:disabled)]:hover:text-brown [&:not(:disabled)]:focus-visible:text-brown [&:not(:disabled)]:hover:bg-light-gray [&:not(:disabled)]:focus-visible:bg-light-gray transition-all">
+                      -
+                    </button>
+                    <p className="">{el.amount}</p>
+                    <button className="py-2 px-3 h-full font-bold hover:text-brown focus-visible:text-brown hover:bg-light-gray focus-visible:bg-light-gray transition-all ">
+                      +
+                    </button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <h2>Cart is empty</h2>
+            )}
+          </div>
+          <div className="">
             <p className="flex justify-between items-center">
               <span className="uppercase opacity-50 tracking-wider">Total</span>
               <span className="uppercase text-lg font-bold tracking-wide">
-                $5325
+                ${data.reduce((a, b) => a + b.price, 0)}
               </span>
             </p>
           </div>
