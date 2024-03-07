485097f9455a1cb2c5d14c7c8e017e5faed7ebde
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-03-07 11:54:44 +0100

    added form html

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,3 +1,43 @@
 export default function Checkout() {
-  return <div className="">checkout</div>;
+  return (
+    <div className="">
+      <div className="">
+        <form className="">
+          <h2>Checkout</h2>
+          <h3>Billing details</h3>
+          <div className="">
+            <label htmlFor="">Name</label>
+            <input type="text" />
+            <label htmlFor="">Email Address</label>
+            <input type="email" />
+            <label htmlFor="">Phone Number</label>
+            <input type="text" />
+          </div>
+          <h3>Shipping info</h3>
+          <div className="">
+            <label htmlFor="">Your Address</label>
+            <input type="text" />
+            <label htmlFor="">ZIP Code</label>
+            <input type="text" />
+            <label htmlFor="">City</label>
+            <input type="text" />
+            <label htmlFor="">Country</label>
+            <input type="text" />
+          </div>
+          <h3>Payment details</h3>
+          <div className="">
+            <label htmlFor="">e-Money</label>
+            <input type="radio" />
+            <label htmlFor="">Cash on Delivery</label>
+            <input type="radio" />
+            <label htmlFor="">e-Money Number</label>
+            <input type="text" />
+            <label htmlFor="">e-Money PIN</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+      <div className=""></div>
+    </div>
+  );
 }
