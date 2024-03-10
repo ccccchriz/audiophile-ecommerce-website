b08ae34bc643661a4faf996695a7bcf81cfd6443
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-03-10 17:06:56 +0100

    checkout styling

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,62 +1,65 @@
 export default function Checkout() {
   return (
-    <div className="p-16  w-full">
+    <div className="py-16 px-6  w-full max-w-[70rem]">
       <div className=" w-full ">
         <form className="bg-white p-6 rounded-lg grid gap-4 w-full">
           <h2 className="font-bold text-3xl uppercase">Checkout</h2>
           <h3 className="text-brown font-bold uppercase">Billing details</h3>
           <div className="grid gap-2">
-            <label htmlFor="" className="font-bold text-sm">
+            <label htmlFor="" className="label-primary">
               Name
             </label>
             <input
+              placeholder="Alexei Ward"
               type="text"
-              className="border-gray border rounded-lg py-4 px-6"
+              className="input-primary"
             />
-            <label htmlFor="" className="font-bold text-sm">
+            <label htmlFor="" className="label-primary">
               Email Address
             </label>
             <input
+              placeholder="alexei@mail.com"
               type="email"
-              className="border-gray border rounded-lg py-4 px-6"
+              className="input-primary"
             />
-            <label htmlFor="" className="font-bold text-sm">
+            <label htmlFor="" className="label-primary">
               Phone Number
             </label>
             <input
+              placeholder="+1 202-555-0136"
               type="text"
-              className="border-gray border rounded-lg py-4 px-6"
+              className="input-primary"
             />
           </div>
           <h3 className="text-brown font-bold uppercase">Shipping info</h3>
           <div className="grid gap-2">
-            <label htmlFor="" className="font-bold text-sm">
+            <label htmlFor="" className="label-primary">
               Your Address
             </label>
             <input
+              placeholder="1137 Williams Avenue"
               type="text"
-              className="border-gray border rounded-lg py-4 px-6"
+              className="input-primary"
             />
-            <label htmlFor="" className="font-bold text-sm">
+            <label htmlFor="" className="label-primary">
               ZIP Code
             </label>
-            <input
-              type="text"
-              className="border-gray border rounded-lg py-4 px-6"
-            />
-            <label htmlFor="" className="font-bold text-sm">
+            <input placeholder="10001" type="text" className="input-primary" />
+            <label htmlFor="" className="label-primary">
               City
             </label>
             <input
+              placeholder="New York"
               type="text"
-              className="border-gray border rounded-lg py-4 px-6"
+              className="input-primary"
             />
-            <label htmlFor="" className="font-bold text-sm">
+            <label htmlFor="" className="label-primary">
               Country
             </label>
             <input
+              placeholder="United States"
               type="text"
-              className="border-gray border rounded-lg py-4 px-6"
+              className="input-primary"
             />
           </div>
           <h3 className="text-brown font-bold uppercase">Payment details</h3>
@@ -65,20 +68,18 @@ export default function Checkout() {
             <input type="radio" />
             <label htmlFor="">Cash on Delivery</label>
             <input type="radio" />
-            <label htmlFor="" className="font-bold text-sm">
+            <label htmlFor="" className="label-primary">
               e-Money Number
             </label>
             <input
-              type="text"
-              className="border-gray border rounded-lg py-4 px-6"
+              placeholder="238521993"
+              type="number"
+              className="input-primary"
             />
-            <label htmlFor="" className="font-bold text-sm">
+            <label htmlFor="" className="label-primary">
               e-Money PIN
             </label>
-            <input
-              type="text"
-              className="border-gray border rounded-lg py-4 px-6"
-            />
+            <input placeholder="6891" type="number" className="input-primary" />
           </div>
         </form>
       </div>
