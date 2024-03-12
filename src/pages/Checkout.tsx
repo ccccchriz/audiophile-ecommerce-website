06621b451023e5f29cc2a31311b089c52feb91c1
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-03-12 19:23:00 +0100

    added summary

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,6 +1,10 @@
+import { getCart, cartType } from "../utilities/CartFunctions.tsx";
+
 export default function Checkout() {
+  const data = getCart() as cartType[];
+
   return (
-    <div className="py-16 px-6  w-full max-w-[70rem]">
+    <div className="py-16 px-6 grid gap-8 w-full max-w-[70rem]">
       <div className=" w-full ">
         <form className="bg-white p-6 rounded-lg grid gap-4 w-full">
           <h2 className="font-bold text-3xl uppercase">Checkout</h2>
@@ -136,7 +140,55 @@ export default function Checkout() {
           </div>
         </form>
       </div>
-      <div className=""></div>
+      <div className="bg-white py-8 px-6">
+        <h2 className="text-lg font-bold uppercase tracking-widest">Summary</h2>
+        <ul className="grid gap-6">
+          {data.map((el, index) => (
+            <li key={index} className="flex gap-4 items-center">
+              <picture className="rounded-lg overflow-hidden">
+                <img src={el.image} alt="" className="w-full max-w-16" />
+              </picture>
+              <div className="grow">
+                <h2 className="font-bold">{el.item}</h2>
+                <p className="tracking-wide font-bold opacity-50">
+                  $ {el.price}
+                </p>
+              </div>
+              <div className="">x{el.amount}</div>
+            </li>
+          ))}
+        </ul>
+        <div className="">
+          <div className="">
+            <h3>Total</h3>
+            <p>
+              {Math.floor(
+                data.reduce((a, b) => a + b.price * b.amount, 0) / 1.23
+              )}
+            </p>
+          </div>
+          <div className="">
+            <h3>Shipping</h3>
+            <p>50</p>
+          </div>
+          <div className="">
+            <h3>Vat (included)</h3>
+            <p>
+              {data.reduce((a, b) => a + b.price * b.amount, 0) -
+                Math.floor(
+                  data.reduce((a, b) => a + b.price * b.amount, 0) / 1.23
+                )}
+            </p>
+          </div>
+        </div>
+        <div className="">
+          <h3>Grand Total</h3>
+          <p>{data.reduce((a, b) => a + b.price * b.amount, 0) + 50}</p>
+        </div>
+        <button className="hover:bg-orange focus-visible:bg-orange text-center text-white uppercase py-4 px-8 font-bold bg-brown tracking-widest w-full">
+          Continue & Pay
+        </button>
+      </div>
     </div>
   );
 }
