248b1a2ef7f70989fc919e201321c992e83d1e43
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-03-18 17:22:36 +0100

    added validation

--- src/utilities/validateForm.tsx
+++ src/utilities/validateForm.tsx
@@ -17,8 +17,10 @@ export default function validateForm(
   if (input?.value == "") {
     input.classList.add("error-border");
     error.textContent = "Name can't be empty";
+  } else if (!input?.value.match(/\S+ \S+/)) {
+    input!.classList.add("error-border");
+    error.textContent = "Make sure to put in a First and Sur-name";
   }
-
   input = event.currentTarget.querySelector("input#mail");
   error = event.currentTarget.querySelector(
     `#${input!.getAttribute("aria-describedby")!}`
@@ -29,6 +31,9 @@ export default function validateForm(
   if (input?.value == "") {
     input.classList.add("error-border");
     error.textContent = "Email Address can't be empty";
+  } else if (!input?.value.match(/\w+@\w+.\w+/)) {
+    input!.classList.add("error-border");
+    error.textContent = "Wrong Format";
   }
 
   input = event.currentTarget.querySelector("input#phone");
@@ -41,5 +46,12 @@ export default function validateForm(
   if (input?.value == "") {
     input.classList.add("error-border");
     error.textContent = "Phone can't be empty";
+  } else if (
+    !input?.value.match(
+      /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+    )
+  ) {
+    input!.classList.add("error-border");
+    error.textContent = "Wrong Format";
   }
 }
